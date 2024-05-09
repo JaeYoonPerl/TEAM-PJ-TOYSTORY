01b0ba317bc43b818dc2e2a492544aee10bcfda4
iron-YUN <wjddbscjf92@naver.com>
2024-05-10 00:56:43 +0900

    로딩페이지후 잘 작동되나요? 깃허브에서 오류남

--- test/js/call_myself.js
+++ test/js/call_myself.js
@@ -23,14 +23,14 @@ function increasePercent() {
   buddy.style.left = ++percent2 + "%";
   // 3.증가숫자가 100보다 작을때까지 계속 재귀호출
   if (percent < 100) {
-    setTimeout(increasePercent, 30);
+    setTimeout(increasePercent, 40);
   } else {
     // 5.바색 변경하기
     bar.style.backgroundColor = "red";
     // 6.슬라이드 이동함수 호출하기
     setTimeout(() => {
       // 페이지 이동 코드 작성
-      window.location.href = "main.html";
+      window.location.href = "./main.html";
     }, 300);
   }///// else /////
 } ////////////// increasePercent ///////////////////
