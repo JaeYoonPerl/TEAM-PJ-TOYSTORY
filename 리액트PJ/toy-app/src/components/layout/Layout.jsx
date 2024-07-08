67655552ef4128e8d775cb8e50999a0a5391acfd
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-07-08 17:40:12 +0900

    dddd

---
+++ 리액트PJ/toy-app/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { FooterArea } from "./FooterArea";
+import MainArea from "./MainArea";
+import { TopArea } from "./TopArea";
+
+function Layout() {
+  return (
+    <>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </>
+  );
+}
+
+export default Layout;
--- 리액트PJ/toy-app/src/components/layout/TopArea.jsx
+++ 리액트PJ/toy-app/src/components/layout/TopArea.jsx
@@ -1,6 +1,48 @@
-export default function TopArea(){
-    // 코드 리턴구역 ////
-    return(
-        <></>
-    );
+import { useEffect } from "react";
+import $ from "jquery";
+export default function TopArea() {
+    useEffect(() => {
+
+            // 대상: .GNB
+            const GNB = $("#GNB");
+            // 대상: .ts
+            const TS = $(".ts");
+            // 대상: .bg-image
+      
+          // GNB 내용
+          let gnbContent = '<ul class="menu-list">';
+          // TS 내용
+          let tsContent = '';
+          for (let i = 1; i <= 4; i++) {
+              gnbContent += `
+              <li data-index="${i}">
+              <a class="icon1" href="./sub${i}.html">Toy Story ${i}</a>
+              <a class="icon2" href="./sub${i}.html">
+                  <img src="./IMG/toy${i}_icon.png" alt="Toy Story ${i}">
+              </a>
+              </li>
+              `;
+              tsContent += `<img src="./IMG/poster/0${i}_ts.jpg" alt="ts${i}" class="ts-img">`;
+          }
+          gnbContent += "</ul>";
+      
+          // 내용 삽입
+          GNB.innerHTML = gnbContent;
+          TS.innerHTML = tsContent;
+    },[]);
+  // 코드 리턴구역 ////
+  return (
+    <>
+      <header id="top-area" class="">
+        <div class="page-bx col-9">
+          <div id="GNB" class=""></div>
+          <div class="logobx-area mlogo">
+            <a href="./main.html">
+              <img src="./IMG/PIXAR_LOGO black.png" alt="메인로고" />
+            </a>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 } /////// TopArea /////
