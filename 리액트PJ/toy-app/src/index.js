0152dc866fa126226930d80c510dce2325ccb38d
JaeYoonLee <a7750430@naver.com>
2024-07-08 16:11:05 +0900

    123

--- 리액트PJ/toy-app/src/index.js
+++ 리액트PJ/toy-app/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Main from "./components/pages/Main";
+import Sub1 from "./components/pages/Sub1";
+import Sub2 from "./components/pages/Sub2";
+import Sub3 from "./components/pages/Sub3";
+import Sub4 from "./components/pages/Sub4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function MainComponent(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
 
+      <Route index element={<Main/>}/>
+      <Route path="sub1" element={<Sub1/>}/>
+      <Route path="sub2" element={<Sub2/>}/>
+      <Route path="sub3" element={<Sub3/>}/>
+      <Route path="sub4" element={<Sub4/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
 
-reportWebVitals();
+// 먼저 root 객체 만들기
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<MainComponent />);
