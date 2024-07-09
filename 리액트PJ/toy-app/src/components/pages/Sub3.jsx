ad32642fb57dc33320460b1ba78c1dac61bacb8e
JaeYoonLee <a7750430@naver.com>
2024-07-09 16:16:15 +0900

    d

--- 리액트PJ/toy-app/src/components/pages/Sub3.jsx
+++ 리액트PJ/toy-app/src/components/pages/Sub3.jsx
@@ -1,9 +1,30 @@
 // import "../../css/sub3.scss";
+
 export default function Sub3(){
     // 코드 리턴구역 ////
     return(
         <>
-         <h1>토이스토리3</h1>
+          <div id="intro-area">
+            <section className="intro-area">
+                <div className="cont-box">
+
+                   
+                    <div className="bg-box"></div>
+                    
+                   
+                    <div className="toy-3rd-logo">
+                        <img src="../images/img3/toystory_logo3-removebg-preview.png" alt="toy3-logo"/>
+                    </div>
+                    
+                    <div className="sino-box">
+                        <span className="sub-text sub-text1">The creators of the beloved Toy Story films re-open the toy box and bring moviegoers back to the delightful world of our favorite gang of toy characters in Toy Story 3. As Andy prepares to depart for college, Buzz, Woody, and the rest of his loyal toys are troubled about their uncertain future. <br/><br/>
+
+                            Toy Story 3 is a comical adventure that lands the toys in a room full of untamed tots who can't wait to get their sticky little fingers on these "new" toys. It's pandemonium as the toys try to stay together, ensuring that "no toy gets left behind."</span>
+                    </div>
+                </div>
+
+            </section>
+        </div>
         </>
     );
 } /////// Sub3 /////
