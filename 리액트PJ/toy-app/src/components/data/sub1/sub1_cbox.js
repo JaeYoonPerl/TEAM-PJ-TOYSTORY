69a932da25b15862eee9b07047c1eb16133e6960
Lee-EUN-KYOUNG <amurca@naver.com>
2024-07-14 20:19:10 +0900

    1111

---
+++ 리액트PJ/toy-app/src/components/data/sub1/sub1_cbox.js
@@ -0,0 +1,22 @@
+import mFn from "../../func/my_function.js";
+
+
+
+export default function sub1Cbox(){
+
+////// 캐릭터 이미지 박스
+// 대상 .cha-box
+const chgrid = mFn.qs(".cha-box");
+
+// 이미지 넣기
+for (let i = 1; i <= 8; i++) {
+  chgrid.innerHTML += `
+      <div>
+        <img src="../../public/images/img1/cha0${i}.jpg" alt="캐릭터">
+        <h3>${sub1_data.chaData[i - 1].chaname}</h3>
+        <p>${sub1_data.chaData[i - 1].chaintro}</p>
+      </div>
+      `;
+
+} //////// for /////////
+}
--- 리액트PJ/toy-app/src/components/modules/S1Character.jsx
+++ 리액트PJ/toy-app/src/components/modules/S1Character.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+// 내함수 불러오기
+import mFn from "../../func/my_function.js";
+
+
+import sub1Cbox from '../data/sub1/sub1_cbox.js';
 
 // CSS 불러오기
 import "../../css/sub1.scss";
 
 function S1Character(prop){
+
+    useEffect(() => {
+    sub1Cbox()
+    });
+
+    ////// 코드 리턴 구역
     return (
         <>
 
