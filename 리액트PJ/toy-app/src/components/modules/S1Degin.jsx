ede54dac47e2d0c32803aaf5c21a98e75c14d764
Lee-EUN-KYOUNG <amurca@naver.com>
2024-07-15 00:41:30 +0900

    111111111

--- 리액트PJ/toy-app/src/components/modules/S1Degin.jsx
+++ 리액트PJ/toy-app/src/components/modules/S1Degin.jsx
@@ -7,43 +7,53 @@ import { designData } from '../data/sub1/sub1_data';
 import "../../css/sub1.scss";
 
 function S1Degin(props) {
+  
+  const S1DeginBox = new Set();
+
   return (
     <>
-      {designData.map((v, i) => (
-        <React.Fragment key={i}>
-          <section className="S1deintro">
-            <div className="S1demovie">
-              <h3>{v.title01}</h3>
-            </div>
-            <div className="S1dbox">
-              <div className="dmovie1">
-                <iframe src={v.mvid01}></iframe>
+      {designData.map((v, i) => {
+        // 중복 체크 (예: title01)
+        if (S1DeginBox.has(v.title01)) return null;
+        S1DeginBox.add(v.title01);
+
+        return (
+          <React.Fragment key={i}>
+            <section className="S1deintro">
+              <div className="S1demovie">
+                <h3>{v.title01}</h3>
               </div>
-              <div className="dmovie2">
-                <iframe src={v.mvid02}></iframe>
-              </div>
-            </div>
-            <h4>{v.subintro01}</h4>
-          </section>
-          <section className="S1bgdintro">
-            <div className="S1degbmovie">
-              <h3>{v.title02}</h3>
-              <div className="S1dbg-box">
-                <div className="dmovie3">
-                  <iframe src={v.mvid03}></iframe>
+              <div className="S1dbox">
+                <div className="dmovie1">
+                  <iframe src={v.mvid01}></iframe>
                 </div>
-                <div className="dmovie4">
-                  <iframe src={v.mvid04}></iframe>
+                <div className="dmovie2">
+                  <iframe src={v.mvid02}></iframe>
                 </div>
-                <h4>{v.subintro02}</h4>
               </div>
-            </div>
-          </section>
-        </React.Fragment>
-      ))}
+              <h4>{v.subintro01}</h4>
+            </section>
+            <section className="S1bgdintro">
+              <div className="S1degbmovie">
+                <h3>{v.title02}</h3>
+                <div className="S1dbg-box">
+                  <div className="dmovie3">
+                    <iframe src={v.mvid03}></iframe>
+                  </div>
+                  <div className="dmovie4">
+                    <iframe src={v.mvid04}></iframe>
+                  </div>
+                  <h4>{v.subintro02}</h4>
+                </div>
+              </div>
+            </section>
+          </React.Fragment>
+        );
+      })}
     </>
   );
 }
+
 
 
 export default S1Degin;
