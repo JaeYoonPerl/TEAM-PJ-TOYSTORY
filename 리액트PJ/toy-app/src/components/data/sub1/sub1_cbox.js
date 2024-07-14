ef2ac9810543184863e9191da640950a3409ab34
Lee-EUN-KYOUNG <amurca@naver.com>
2024-07-14 20:31:13 +0900

    1111

--- 리액트PJ/toy-app/src/components/data/sub1/sub1_cbox.js
+++ 리액트PJ/toy-app/src/components/data/sub1/sub1_cbox.js
@@ -7,16 +7,16 @@ export default function sub1Cbox(){
 ////// 캐릭터 이미지 박스
 // 대상 .cha-box
 const chgrid = mFn.qs(".cha-box");
-
+let chcode = "";
 // 이미지 넣기
 for (let i = 1; i <= 8; i++) {
-  chgrid.innerHTML += `
+  chcode += `
       <div>
         <img src="../../public/images/img1/cha0${i}.jpg" alt="캐릭터">
         <h3>${sub1_data.chaData[i - 1].chaname}</h3>
         <p>${sub1_data.chaData[i - 1].chaintro}</p>
       </div>
       `;
-
-} //////// for /////////
+  }
+  chgrid.innerHTML =chcode;
 }
