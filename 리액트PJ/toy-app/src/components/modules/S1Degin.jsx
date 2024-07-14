d02115a3b8205eff476f27bec681ed128c49bfc6
Lee-EUN-KYOUNG <amurca@naver.com>
2024-07-15 00:35:40 +0900

    111111111111

--- 리액트PJ/toy-app/src/components/modules/S1Degin.jsx
+++ 리액트PJ/toy-app/src/components/modules/S1Degin.jsx
@@ -10,41 +10,40 @@ function S1Degin(props) {
   return (
     <>
       {designData.map((v, i) => (
-        <section key={i} className="S1deintro">
-          <div className="S1demovie">
-            <h3>{v.title01}</h3>
-          </div>
-          <div className="S1dbox">
-            <div className="dmovie1">
-              <iframe src={v.mvid01}></iframe>
+        <React.Fragment key={i}>
+          <section className="S1deintro">
+            <div className="S1demovie">
+              <h3>{v.title01}</h3>
             </div>
-            <div className="dmovie2">
-              <iframe src={v.mvid02}></iframe>
-            </div>
-          </div>
-          <h4>{v.subintro01}</h4>
-        </section>
-      ))}
-      
-      {designData.map((v, i) => (
-        <section key={i} className="S1bgdintro">
-          <div className="S1degbmovie">
-            <h3>{v.title02}</h3>
-            <div className="S1dbg-box">
-              <div className="dmovie3">
-                <iframe src={v.mvid03}></iframe>
+            <div className="S1dbox">
+              <div className="dmovie1">
+                <iframe src={v.mvid01}></iframe>
               </div>
-              <div className="dmovie4">
-                <iframe src={v.mvid04}></iframe>
+              <div className="dmovie2">
+                <iframe src={v.mvid02}></iframe>
               </div>
-              <h4>{v.subintro02}</h4>
             </div>
-          </div>
-        </section>
+            <h4>{v.subintro01}</h4>
+          </section>
+          <section className="S1bgdintro">
+            <div className="S1degbmovie">
+              <h3>{v.title02}</h3>
+              <div className="S1dbg-box">
+                <div className="dmovie3">
+                  <iframe src={v.mvid03}></iframe>
+                </div>
+                <div className="dmovie4">
+                  <iframe src={v.mvid04}></iframe>
+                </div>
+                <h4>{v.subintro02}</h4>
+              </div>
+            </div>
+          </section>
+        </React.Fragment>
       ))}
     </>
   );
-  
 }
+
 
 export default S1Degin;
