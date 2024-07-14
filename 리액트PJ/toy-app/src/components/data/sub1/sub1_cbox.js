bfe12de0339529ab46fd2f01ccd053330a6a7e0e
Lee-EUN-KYOUNG <amurca@naver.com>
2024-07-14 21:25:06 +0900

    11

--- 리액트PJ/toy-app/src/components/data/sub1/sub1_cbox.js
+++ 리액트PJ/toy-app/src/components/data/sub1/sub1_cbox.js
@@ -9,7 +9,7 @@ export default function sub1Cbox(ref) {
     for (let i = 1; i <= 8; i++) {
       chgrid.innerHTML += `
         <div class="s1chbox">
-          <img src="../../public/images/img1/cha0${i}.jpg" alt="캐릭터">
+          <img src="../public/images/img1/cha0${i}.jpg" alt="캐릭터">
           <h3>${chaData.chaname}</h3>
           <p>${chaData.chaintro}</p>
         </div>
