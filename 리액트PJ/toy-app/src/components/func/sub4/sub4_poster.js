cd4b6014ea60ea3a477a7e361e502cd977e7cf7d
JaeYoonLee <a7750430@naver.com>
2024-07-15 18:20:01 +0900

    Update sub4_poster.js

--- 리액트PJ/toy-app/src/components/func/sub4/sub4_poster.js
+++ 리액트PJ/toy-app/src/components/func/sub4/sub4_poster.js
@@ -7,7 +7,7 @@ function sub4_poster(props) {
     
     const poster = mFn.qsa('.poster-common');
 
-    mFn.addEvt(poster[0],"mouseenter",sub4_poster)
+    mFn.addEvt(poster[0],"mouseenter",sub4_poster);
 
     this.parentElement.classList.add("on");
     // 2초후 앞에 있는 포스터 없애기
