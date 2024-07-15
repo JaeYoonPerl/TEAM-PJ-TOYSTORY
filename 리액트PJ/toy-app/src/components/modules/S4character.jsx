49f11c4ba9c9c4b6fe2d9f849ece7cd05e29906c
doyeun93 <doyeun93@naver.com>
2024-07-15 12:28:22 +0900

    s

--- 리액트PJ/toy-app/src/components/modules/S4character.jsx
+++ 리액트PJ/toy-app/src/components/modules/S4character.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import { catInfo } from '../data/sub4/sub4_chadata';
+import { catInfo } from "../data/sub4/sub4_chadata";
 
 function S4character(props) {
-    let cData = catInfo;
+  let cData = catInfo;
 
-    return (
-        <div className="cha-box">
-            {cData.map((v,i) => (
-                <ul className="cha-pic" key={i}>
-                    <li>
-                        <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}`} alt={v.title} />
-                    </li>
-                    <h1 className="main-title">{v.title}</h1>
-                    <span className="sub-text2">{v.desc}</span>
-                </ul>
+  return (
+    <div className="cha-intro">
+      {cData.map((v, i) => (
+        <ul className="cha-pic" key={i}>
+          <li>
+            <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}1.jpg`} alt={v.title} />
+          </li>
+          <li>
+            <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}2.jpg`} alt={v.title} />
+          </li>
+          <li>
+            <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}3.jpg`} alt={v.title} />
+          </li>
+          <li>
+            <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}4.jpg`} alt={v.title} />
+          </li>
+          <li>
+            <img src={process.env.PUBLIC_URL + `/images/img4/${v.imgName}5.jpg`} alt={v.title} />
+          </li>
+          <h1 className="main-title">{v.title}</h1>
+          <span>
+            {v.desc.split("*").map((v, i) => (
+              <p key={i}>{v}</p>
             ))}
-        </div>
-    );
+          </span>
+          {/* <span className="sub-text2">{v.desc}</span> */}
+        </ul>
+      ))}
+    </div>
+  );
 }
 
 export default S4character;
