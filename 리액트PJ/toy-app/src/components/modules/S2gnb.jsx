83fff955b7ded22ba47747bbcab49fc6d20f473e
iron-YUN <wjddbscjf92@naver.com>
2024-07-16 02:53:08 +0900

    못해먹겠네

--- 리액트PJ/toy-app/src/components/modules/S2gnb.jsx
+++ 리액트PJ/toy-app/src/components/modules/S2gnb.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react';
-// 데이터 불러오기
 import { gnbItems2 } from "../func/sub2/sub2_data";
-// 내함수 불러오기
-import mFn from "../func/my_function";
 
-function S2gnb() {
-    const [selectedIdx, setSelectedIdx] = useState(0);
-    useEffect(()=>{
-        const gnbLi = mFn.qsa(".gnb-ul li");
-        gnbLi[0].classList.add("on");
-    },[selectedIdx]);
+const S2gnb = () => {
 
     return (
-        <ul className='gnb-ul'>
+        <ul className="gnb-ul">
             {
-                gnbItems2.map((v,i)=>
-                    <li>
-                <a href="#">{v.tit}</a>
-              </li>
-                )
+                gnbItems2.map((v, i) => (
+                    <li key={i}>
+                       <a href={`#${v.link}`}>{v.tit}</a>
+                    </li>
+                ))
             }
         </ul>
     );
